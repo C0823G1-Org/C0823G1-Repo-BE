366dca42a1d4abd812107810f8aa8ae4ceaa6507
LamTamNhu <46390185+LamTamNhu@users.noreply.github.com>
2023-12-08 00:31:29 +0700

    *update cart procedures and .gitignore

--- mysql_db/store_procedure.sql
+++ mysql_db/store_procedure.sql
@@ -1,7 +1,7 @@
 delimiter //
 create procedure get_user_cart(user_id_request int)
 begin
-select g.game_title,g.price,i.url
+select g.game_title,g.price,i.url,g.game_id
 from game_in_cart as c
 join user as u on c.user_id = u.user_id
 join game as g on c.game_id= g.game_id
@@ -9,3 +9,18 @@ join image as i on i.game_game_id = g.game_id
 where c.user_id = user_id_request and i.image_type_image_type_id=2 and c.is_deleted=0;
 end
 // delimiter ;
+
+delimiter //
+create procedure remove_cart_item(request_user_id int,request_game_id int)
+begin
+update game_in_cart as c
+join user as u on c.user_id=u.user_id
+set c.is_deleted=1
+where  c.game_id=request_game_id and u.user_id=request_user_id;
+end
+// delimiter ;
+
+select *
+from game_in_cart c
+join user as u on c.user_id = c.user_id
+where c.game_id=2 and u.user_id=1
