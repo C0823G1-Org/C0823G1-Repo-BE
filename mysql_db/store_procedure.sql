602f008bcbf8d6816db9195525d9b914101be1ea
LamTamNhu <46390185+LamTamNhu@users.noreply.github.com>
2023-12-13 21:59:44 +0700

    Update store_procedure.sql

--- mysql_db/store_procedure.sql
+++ mysql_db/store_procedure.sql
@@ -20,6 +20,15 @@ where  c.game_id=request_game_id and u.user_id=request_user_id;
 end
 // delimiter ;
 
+delimiter //
+create procedure get_game_for_cart(game_id_request int)
+begin
+select g.game_title,g.price,i.url,g.game_id
+from game as g
+left join image as i on i.game_game_id=g.game_id
+where i.is_deleted=0 and game_id_request=g.game_id and i.image_type_image_type_id=2;
+end
+// delimiter ;
 select *
 from game_in_cart c
 join user as u on c.user_id = c.user_id
@@ -31,13 +40,13 @@ delimiter //
 CREATE PROCEDURE update_all(game_id int ,game_title text,price double,description text,age_requirement int ,developer_id int ,release_date date,dlc_id int,review_rating_id int,`minium_system_requirement` text,`recommend_system_requirement`text)
 BEGIN
 update game set game_title = game_title , price = price , reviewRatingId = reviewRatingId where game_title = game_title;
- END 
+ END
  // delimiter  ;
 
 =======
 delimiter //
 create procedure create_user_account()
-begin 
+begin
 select u.user_id,u.user_name, u.birthday, u.email
 from `user` u
 join user_account ua on u.email = ua.email
